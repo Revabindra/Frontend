4d76e114a4bfb27c3a55770f4279513c9713916d
ravneet <revabindra23@gmail.com>
2025-07-07 14:41:18 +0530

    Local changes before pull

--- class/7july.js
+++ class/7july.js
@@ -26,17 +26,81 @@ console.log(result);
 //promises - are object which is having status, response  and it is asynchronous 
 //status -pending, fulfilled and rejected
 // promise - .then(fulfilled) and .catch(rejected)
-const promise = new Promise((resolve, reject) =>{
+// const promise = new Promise((resolve, reject) =>{
+//     setTimeout(() =>{
+//         resolve("Promise is resolved");
+//     },3000);
+// });
+// const data=promise
+// .then((res)=>{
+//     console.log(res);
+// })
+// .then(
+//     console.log("punal get icr")
+// )
+// .then(
+//     console.log("punal working")
+// )
+// .then(
+//     console.log("punal layoff")
+// )
+// .catch((err)=>{
+//     console.log(err);
+// });
+// console.log(data);
+
+const promise1 = new Promise((resolve, reject) =>{
     setTimeout(() =>{
-        resolve("Promise is resolved");
-    },3000);
+        reject("promise 1");
+   },3000);
 });
-const data=promise
-.then((res)=>{
-    console.log(res);
-})
-.catch((err)=>{
-    console.log(err);
-})
-console.log(data);
+const promise2 = new Promise((resolve, reject) =>{
+   setTimeout(() =>{
+        reject("promise 2");
+    },5000);
+});
+const promise3 = new Promise((resolve, reject) =>{
+    setTimeout(() =>{
+        reject("promise 3");
+   },6000);
+});
+const promise4 = new Promise((resolve, reject) =>{
+   setTimeout(() =>{
+        reject("promise 4");
+    },7000);
+});
+Promise.all([promise1,promise2,promise3,promise4])
+ .then((results) => {
+        console.log("Promise.all:", results);
+    })
+    .catch((err) => {
+        console.log("Promise.all Error:", err);
+    });
+Promise.allSettled([promise1,promise2,promise3,promise4])
+ .then((results) => {
+        console.log("Promise.allsettled:", results);
+    })
+    .catch((err) => {
+        console.log("Promise.allsettled Error:", err);
+    });
+Promise.any([promise1,promise2,promise3,promise4])
+ .then((results) => {
+        console.log("Promise.any:", results);
+    })
+    .catch((err) => {
+        console.log("Promise.any Error:", err);
+    });
+Promise.race([promise1,promise2,promise3,promise4])
+ .then((results) => {
+        console.log("Promise.race:", results);
+    })
+    .catch((err) => {
+        console.log("Promise.race Error:", err);
+    });
+
+    
+
+//promise api's
+//promise.any, promise.all, promise.race , promise.settled
+
 
