74a293bb8000838fb169eee1386053af58d63237
ravneet <revabindra23@gmail.com>
2025-07-09 18:53:40 +0530

    Add Assignment1 folder and remove old files

---
+++ Assignment1/script.js
@@ -0,0 +1,26 @@
+  async function fetchData() {
+      const res = await fetch('https://dummyjson.com/products');
+      const data = await res.json();
+      console.log(data);
+      return data.products; 
+    }
+
+    async function renderProducts() {
+      const container = document.getElementById("container");
+      const products = await fetchData();
+
+      products.forEach(product => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        card.innerHTML = `
+          <img src="${product.thumbnail}" alt="${product.title}">
+          <h2>${product.title}</h2>
+          <p>${product.description}</p>
+          <p class="price">₹${product.price}</p>
+        `;
+
+        container.appendChild(card);
+      });
+    }
+    renderProducts();
