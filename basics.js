24dc34c7e433102df0ec41a8c11205464819660d
ravneet <revabindra23@gmail.com>
2025-07-19 15:33:55 +0530

    commit

--- basics.js
+++ basics.js
@@ -1,18 +1,18 @@
-console.log(x);
-var x=7;
+// console.log(x);
+// var x=7;
 
 //const x=9;    //more strict than other two
 //let x=7;      //more strict than var and can't be reused again 
 //var x=10;      //less strict than let and can be declared again 
 
-const id= 123
-let name="reva kaur"
-var password="12345"
-email="revabindra"
+// const id= 123
+// let name="reva kaur"
+// var password="12345"
+// email="revabindra"
 
-name="archi"       //updated the name
-password="1234567" //updated the password
-console.table([id,name,password])   // table form 
+// name="archi"       //updated the name
+// password="1234567" //updated the password
+// console.table([id,name,password])   // table form 
 //var not used due to block scope and functional scope issue 
 
 "use strict"         // treat all js code as new version 
@@ -26,42 +26,42 @@ console.table([id,name,password])   // table form
 //symbol unique
 //object 
 
-console.log(typeof null); // object 
-console.log(typeof undefined); //undefined
+// console.log(typeof null); // object 
+// console.log(typeof undefined); //undefined
 
 //"33" = 33
 //"33abc" =Nan
 // true = 1, false=0 , "" false, 
 
 //operations ----------------------------------------------------------------------------------
-let str1="reva"
-let str2="kaur"
-let str3=str1+str2 //revakaur
-console.log(str3);
+// let str1="reva"
+// let str2="kaur"
+// let str3=str1+str2 //revakaur
+// console.log(str3);
 
-console.log("1"+2) //12
-console.log(1+"2") //12
-console.log("1"+2+3)// 123
-console.log(1+2+"3") //33
+// console.log("1"+2) //12
+// console.log(1+"2") //12
+// console.log("1"+2+3)// 123
+// console.log(1+2+"3") //33
 
-console.log(null>0)//false
-console.log(null>=0)//true
-console.log(null==0)//false
+// console.log(null>0)//false
+// console.log(null>=0)//true
+// console.log(null==0)//false
 //> ,< convert null to 0 
 
-console.log(undefined>0)//false
-console.log(undefined>=0)//false
-console.log(undefined==0)//false
+// console.log(undefined>0)//false
+// console.log(undefined>=0)//false
+// console.log(undefined==0)//false
 
-console.log("2"==2);   //true
-console.log("2"===2);  //false
+// console.log("2"==2);   //true
+// console.log("2"===2);  //false
 
 // datatypes how datatypes in memory processed 
 //primitive(call by value) string number boolean null(empty) undefined 
 //symbol(unique value to make)
-// const id=Symbol('abc')
-// const anid=Symbol('abc')
-// console.log(id===anid)      //FALSE
+const id=Symbol('abc')
+const anid=Symbol('abc')
+console.log(id==anid)      //FALSE
 // bigint
 //non primitive (call by reference) array,object, function
 // TYPE OF NULL = OBJECT 
