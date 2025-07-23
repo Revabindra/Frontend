4b0c9906104089905b33942ec89ca4febd52b72a
ravneet <revabindra23@gmail.com>
2025-07-23 15:38:37 +0530

    commit

---
+++ class/21july.js
@@ -0,0 +1,19 @@
+// node package manager and plugins 
+// npm init - to initialize package and got package.json 
+// version control - 1.0.0 and 1 major change and 0 minor change 
+// last 0 is patch/bump and it will be increasing always one by one 
+// 1.0.1 1.0.2 1.0.3 then 1.1.0 1.1.1
+// npm init 
+//npm login 
+// npm publish 
+
+
+const sum=(()=>{
+    console.log("this is sum");
+})
+
+
+//change set - manages the version 
+//npx changeset 
+//npx changeset version
+
---
+++ class/CHANGELOG.md
@@ -0,0 +1,13 @@
+# g6-package
+
+## 1.0.3
+
+### Patch Changes
+
+- I am changing the version to another level now
+
+## 1.0.2
+
+### Patch Changes
+
+- I ADDED THE CHANGE OF DIV
